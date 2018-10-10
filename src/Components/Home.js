a118e0d90e2743248aa3a6380c28c085804b8009
Mithun Singh <mithun.singh@cerner.com>
2018-10-09 21:38:56 -0500

    Initial version

---
+++ src/Components/Home.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import GetTodos from './GetTodos';
+
+const ACCESS_TOKEN = localStorage.getItem('access_token');
+export const client = new ApolloClient({
+  uri: "https://ms-todoapp.herokuapp.com/v1alpha1/graphql",
+  headers: {
+    Authorization: `Bearer ${ACCESS_TOKEN}`,
+  }
+});
+
+class Home extends Component {
+  login() {
+    this.props.auth.login();
+  }
+  render() {
+    const { isAuthenticated } = this.props.auth;
+    return (
+      isAuthenticated() && (
+        <ApolloProvider client={client}>
+          <div className="container">
+            <GetTodos />           
+          </div>
+        </ApolloProvider>
+      )
+    );
+  }
+}
+
+export default Home;
